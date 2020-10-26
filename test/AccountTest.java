cf161c2e6c281ef0975085376e6e108c79eaa499
Alex Glushko <alextvrus@gmail.com>
2020-10-26 22:01:38 +0300

    Initial commit

---
+++ test/AccountTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMaxCredit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isBlocked() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void block() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void unblock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setMaxCredit() {
+    }
+}
